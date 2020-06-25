77611d94fc3d845bc9bd0ec48792666197eb1bda
nick <nick_php@163.com>
2020-06-25 17:36:25 +0800

    盛最多水的容器

--- Week_01/climbStairs.php
+++ Week_01/climbStairs.php
@@ -1,13 +1,16 @@
 <?php
 
+/**
+ *  1 2 3 5 8 13
+ */
 function climbStairs($n) {
-    if (count($n) < 2) {
+    if ($n <= 2) {
         return $n;
     }
     $f1 = 1;
     $f2 = 2;
     $f3 = 3;
-    for ($i = 3; $i <= count($n); $i++) {
+    for ($i = 3; $i <= $n; $i++) {
         $f3 = $f1 + $f2;
         $f1 = $f2;
         $f2 = $f3;
@@ -15,4 +18,4 @@ function climbStairs($n) {
     return $f3;
 }
 
-echo climbStairs(1);
+echo climbStairs(10);
--- Week_01/maxArea.php
+++ Week_01/maxArea.php
@@ -10,12 +10,30 @@
 // 	return $max;
 // }
 
+// function maxArea($arr) {
+//     $max = 0;
+//     for ($i = 0; $i < count($arr) - 1; $i++) {
+//         for ($j = $i + 1; $j < count($arr); $j++) { 
+//            $area = ($j - $i) * min($arr[$i], $arr[$j]);
+//            $max = max($max, $area);
+//         }
+//     }
+//     return $max;
+// }
+
 function maxArea($arr) {
+    if (count($arr) < 2) {
+        return $arr;
+    }
     $max = 0;
-    for ($i = 0; $i < count($arr) - 1; $i++) {
-        for ($j = $i + 1; $j < count($arr); $j++) { 
-           $area = ($j - $i) * min($arr[$i], $arr[$j]);
-           $max = max($max, $area);
+    $left = 0;
+    $right = count($arr) - 1;
+    while ($left < $right) {
+        $max = max($max, ($right - $left) * min($arr[$left], $arr[$right]));
+        if ($arr[$left] < $arr[$right]) {
+            $left++;
+        } else {
+            $right--;
         }
     }
     return $max;
