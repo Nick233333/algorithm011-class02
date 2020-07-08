59061317f1e63de490e549d9b5412732f979a66b
nick <nick_php@163.com>
2020-07-08 23:18:06 +0800

    翻转二叉树

---
+++ Week_03/invertTree.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return TreeNode
+     */
+    function invertTree($root) {
+        if ($root == null) {
+            return $root;
+        }
+        $left = $root->left;
+        $right = $root->right;
+        $root->left = $this->invertTree($right);
+        $root->right = $this->invertTree($left);
+        return $root;
+    }
+}
